f2df81bca7b2e7f29f4cb9950ff411c6d818feeb
Tiago Royer <royertiago@gmail.com>
2015-04-10 13:12:44 -0300

    Add test/cmdline_args.test.cpp.

---
+++ test/cmdline_args.test.cpp
@@ -0,0 +1,43 @@
+/* Unit test for the class cmdline::args.
+ */
+#include "cmdline/args.h"
+#include <catch.hpp>
+
+TEST_CASE( "Basic cmdline::args test", "[cmdline][trivial]" ) {
+    const char * argv[] = {
+        "test",
+        "--home",
+        "--val",
+        "45",
+        "--",
+        "file.cpp",
+        "another.cpp",
+    };
+    cmdline::args args( 7, argv );
+
+    CHECK( args.size() == 6 );
+
+    CHECK( args.peek() == "--home" );
+    CHECK( args.next() == "--home" );
+    CHECK( args.size() == 5 );
+
+    CHECK( args.peek() == "--val" );
+    CHECK( args.next() == "--val" );
+    CHECK( args.size() == 4 );
+
+    CHECK( args.peek() == "45" );
+    CHECK( args.next() == "45" );
+    CHECK( args.size() == 3 );
+
+    CHECK( args.peek() == "--" );
+    args.shift();
+    CHECK( args.size() == 2 );
+
+    CHECK( args.peek() == "file.cpp" );
+    args.shift();
+    CHECK( args.size() == 1 );
+
+    CHECK( args.peek() == "another.cpp" );
+    args.shift();
+    CHECK( args.size() == 0 );
+}
